1c071d435236406312764dcb183530ce7391afb4
Eduardo Calderon <ecalderonn@outlook.es>
2024-10-08 08:05:16 -0500

    Editando la creqacion y la lista de compras, asi com la eliminacion y edicion de las compras

--- src/routes/compras.routes.js
+++ src/routes/compras.routes.js
@@ -3,22 +3,6 @@ import pool from '../database.js'
 
 const router = Router();
 
-router.get('/comadd', async(req,res)=>{
-    res.render('compras/comadd');
-});
 
-router.post('/comadd', async(req, res)=>{
-    try{
-        const {fecha_com, cantidad_com, id_tp_com, id_pro_com} = req.body;
-        const newBuy = {
-            fecha_com, cantidad_com, id_tp_com, id_pro_com
-        }
-        await pool.query('INSERT INTO compra SET ?', [newBuy]);
-        res.redirect('/comadd');
-    }
-    catch(err){
-        res.status(500).json({message:err.message});
-    }
-});
 
 export default router;
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -68,4 +68,74 @@ router.get('/proddelete/:id_pro', async(req, res)=>{
     }
 });
 
+// poner aqui el router de la compra desde aqui
+
+router.get('/compracrear', async(req,res)=>{
+    const [resultper] = await pool.query('SELECT * FROM personas');
+    const [resultpro] = await pool.query('SELECT * FROM productos');
+    res.render('compras/compracrear', {productos:resultpro, personas:resultper});
+});
+
+router.post('/compracrear', async(req, res)=>{
+    try{
+        const {cantidad_com, fecha_com, id_p_com, id_pro_com} = req.body;
+        const newCompra = {
+            cantidad_com, fecha_com, id_p_com, id_pro_com
+        }
+        await pool.query('INSERT INTO compras SET ?', [newCompra]);
+        res.redirect('/compralistar');
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+});
+
+router.get('/compralistar', async(req, res)=>{
+    try{
+        const [result] = await pool.query('SELECT * FROM personas p, compras c, productos pro WHERE id_p_com = id AND id_pro_com = id_pro;');
+        res.render('compras/compralistar', {compras: result});
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+});
+
+router.get('/compraedit/:id_com', async(req, res)=>{
+    try{
+        const {id_com} = req.params;
+        const [compras] = await pool.query('SELECT * FROM compras WHERE id_com = ?', [id_com]);
+        const compraEdit = compras[0];
+        res.render('compras/compraeditar', {compras: compraEdit});
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+});
+
+router.post('/compraedit/:id_com', async(req, res)=>{
+    try{
+        const {cantidad_com, fecha_com, id_p_com, id_pro_com} = req.body;
+        const {id_com} = req.params;
+        const editCompra = {
+            cantidad_com, fecha_com, id_p_com, id_pro_com
+        };
+        await pool.query('UPDATE compras SET ? WHERE id_pro = ?', [editCompra, id_pro]);
+        res.redirect('/compralistar');
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+});
+
+router.get('/compradelete/:id_com', async(req, res)=>{
+    try{
+        const {id_com} = req.params;
+        await pool.query('DELETE FROM compras WHERE id_com = ?', [id_com]);
+        res.redirect('/compralistar');
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+});
+
 export default router;
